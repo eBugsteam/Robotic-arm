0d1867e72776509a732134a203d93ede529e8558
eBugsteam <123866142+eBugsteam@users.noreply.github.com>
2024-03-02 11:53:27 +0330

    Update Bluetooth_demo.ino
    servo 2 and 3 added

--- Bluetooth_demo.ino
+++ Bluetooth_demo.ino
@@ -6,31 +6,45 @@ String str = " ";
 
 void setup() {
   // Start the serial communication
-  Serial.begin(9600);  
-  
-   if (Serial.available() > 0)
-   
-  Serial.print("\tBluetooth Connected Seccessfully.\a");// Checks whether data is comming from the serial port
+  Serial.begin(9600);
+
+  if (Serial.available() > 0)
+
+    Serial.print("\tBluetooth Connected Seccessfully.\a");  // Checks whether data is comming from the serial port
 }
 
 void loop() {
   if (Serial.available() > 0) {
     // Read the incoming string
     str = Serial.readString();
+
+    // Servo 1
     if (str.startsWith("S1")) {
       str = str.substring(3, str.length());
       int S1 = str.toInt();
-      //s1 = map(s1, 20, 186, 0, 180)
       Serial.print(S1);
-
-  
-
-    
     }
 
+    // Servo 2
+    if (str.startsWith("S2")) {
+      str = str.substring(3, str.length());
+      int S2 = str.toInt();
+      Serial.print(S2);
+    }
+
+    // Servo 3
+    if (str.startsWith("S3")) {
+      str = str.substring(3, str.length());
+      int S3 = str.toInt();
+      Serial.print(S3);
+    }
+
+
+
+
     // Remove any whitespace or newline characters
-   // str.trim();
-    
+    // str.trim();
+
     // Convert the string to an integer
     int num = str.toInt();
 
