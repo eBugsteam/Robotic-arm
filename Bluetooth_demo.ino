02c8bb488a46e8334b915a3e29df8e92e617a511
eBugsteam <123866142+eBugsteam@users.noreply.github.com>
2024-03-02 11:35:13 +0330

    Create Bluetooth_demo.ino

---
+++ Bluetooth_demo.ino
@@ -0,0 +1,42 @@
+#include <Wire.h>
+
+
+
+String str = " ";
+
+void setup() {
+  // Start the serial communication
+  Serial.begin(9600);  
+  
+   if (Serial.available() > 0)
+   
+  Serial.print("\tBluetooth Connected Seccessfully.\a");// Checks whether data is comming from the serial port
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    // Read the incoming string
+    str = Serial.readString();
+    if (str.startsWith("S1")) {
+      str = str.substring(3, str.length());
+      int S1 = str.toInt();
+      //s1 = map(s1, 20, 186, 0, 180)
+      Serial.print(S1);
+
+  
+
+    
+    }
+
+    // Remove any whitespace or newline characters
+   // str.trim();
+    
+    // Convert the string to an integer
+    int num = str.toInt();
+
+    // Print the integer to the serial monitor
+    Serial.print("The converted number is: ");
+    Serial.println(num);
+  }
+  delay(100);
+}
