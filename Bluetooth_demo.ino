26fc887b4726f65aae470e1a20594d63548b6de1
eBugsteam <123866142+eBugsteam@users.noreply.github.com>
2024-03-03 09:41:46 +0330

    Update Bluetooth_demo.ino

--- Bluetooth_demo.ino
+++ Bluetooth_demo.ino
@@ -1,49 +1,33 @@
 #include <Wire.h>
+#include <Servo.h>
 
+Servo servo;
 
-
-String str = " ";
 
 void setup() {
   // Start the serial communication
-  Serial.begin(9600);
+  Serial.begin(9600);  
+servo.attach(3);
+   if (Serial.available() > 0)
 
-  if (Serial.available() > 0)
-
-    Serial.print("\tBluetooth Connected Seccessfully.\a");  // Checks whether data is comming from the serial port
+  Serial.print("\tBluetooth Connected Seccessfully.\a");// Checks whether data is comming from the serial port
 }
 
 void loop() {
   if (Serial.available() > 0) {
     // Read the incoming string
-    str = Serial.readString();
-
-    // Servo 1
+    String str = Serial.readString();
     if (str.startsWith("S1")) {
       str = str.substring(3, str.length());
       int S1 = str.toInt();
+      //s1 = map(s1, 20, 186, 0, 180)
       Serial.print(S1);
+      servo.write(S1);
+
     }
-
-    // Servo 2
-    if (str.startsWith("S2")) {
-      str = str.substring(3, str.length());
-      int S2 = str.toInt();
-      Serial.print(S2);
-    }
-
-    // Servo 3
-    if (str.startsWith("S3")) {
-      str = str.substring(3, str.length());
-      int S3 = str.toInt();
-      Serial.print(S3);
-    }
-
-
-
 
     // Remove any whitespace or newline characters
-    // str.trim();
+   // str.trim();
 
     // Convert the string to an integer
     int num = str.toInt();
