380386d09bd1c7a68b4efa539f154a54c1b09023
eBugsteam <123866142+eBugsteam@users.noreply.github.com>
2024-03-04 11:27:51 +0330

    Update final.ino

--- Codes/final.ino
+++ Codes/final.ino
@@ -1,52 +1,34 @@
-//Libraries
 #include <Wire.h>
-#include <MPU6050.h>
 #include <Servo.h>
+#include <MPU6050.h>
 
 
-// Pin defines
-//sero
-#define servobase_pin 3
-#define servomid1_pin 5
-#define servomid2_pin 6
-#define servorot_pin 9
-#define servogrip_pin 10
-//sensor
 #define sensor0 A0
 #define sensor1 A1
 #define sensor2 A2
 #define sensor3 A3
 
-
-//Initialize
-Servo servobase, servomid1, servomid2, servorot, servogrip;
-MPU6050 sensor;
-
-//Global Variables 
 double sensor0_value, sensor1_value, sensor2_value, sensor3_value;
+
+
 float glove_average = 0;
-float gripper_degree = 0;
+float gripper_value = 0;
 int16_t ax, ay, az, roll_angle, pitch_angle;
 int16_t gx, gy, gz;
 
-void setup()
-{
+Servo servo;
+Servo servo1;
+Servo servo2;
+MPU6050 sensor;
+
+void setup() {
+  // put your setup code here, to run once:
   Serial.begin(9600);
   Wire.begin();
-
-
-
-  servogrip.attach(servogrip_pin);
-  servogrip.write(170);
-  servorot.attach(servorot_pin);
-  servorot.write(0);
-  servomid2.attach(servomid2_pin);
-  servomid2.write(0);
-
-
-
-
-
+  servo1.attach(3);
+  servo2.attach(4);
+  servo.attach(7);
+  servo.write(170);
   Serial.println("Initializing the sensor");
   sensor.initialize();
   Serial.println(sensor.testConnection() ? "Successfully Connected" : "Connection failed!");
@@ -55,46 +37,64 @@ void setup()
   delay(1000);
 }
 
+void loop() {
 
-void loop()
-{
   sensor.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
 
   roll_angle = map(ax, -17000, 17000, 0, 180);
-  pitch_angle = map(ay, -17000, 17000, 0, 180);
+ // pitch_angle = map(ay, -17000, 17000, 0, 180);
 
-  //servo4.write(pitch_angle);
-  servorot.write(roll_angle);
-  servomid2.write(pitch_angle);
+  servo1.write(roll_angle);
 
-  sensor0_value = analogRead(sensor0);
-  sensor1_value = analogRead(sensor1);
+  sensor0_value = floor(analogRead(sensor0) / 100);
+  sensor1_value = floor(analogRead(sensor1) / 100);
   sensor2_value = analogRead(sensor2);
   sensor3_value = analogRead(sensor3);
 
-  glove_average = (sensor0_value + sensor1_value) / 2.0; // Changed to floating point for precision
-  gripper_degree = map(glove_average, 0, 1023, 170, 10); // Assuming 10-bit ADC. Change 1023 if bit depth is different.
+  glove_average = (sensor0_value + sensor1_value) / 2;
+
+  gripper_value = map(glove_average, 0, 4, 170, 10);
   
-  servogrip.write(gripper_degree);
 
-  //Printing Data for Serial Plotter
-  Serial.print("Roll-Angle:");
+  servo.write(gripper_value);
+
+  if (Serial.available() > 0) {
+    // Read the incoming string
+    String str = Serial.readString();
+    if (str.startsWith("S1")) {
+      str = str.substring(3, str.length());
+      int S1 = str.toInt();
+      S1 = map(S1, 20, 186, 0, 180);
+      //Serial.print(S1);
+      //S1 = map(S1, 0, 180, 0, 1023);
+      //analogWrite(2, S1);
+      servo2.write(S1);
+    }
+
+    int num = str.toInt();
+
+    // Print the integer to the serial monitor
+    Serial.print("The converted number is: ");
+    //Serial.println(num);
+  }
+
+  Serial.print("Sensor1: ");
+  Serial.print(floor(sensor0_value));
+  Serial.print("\t");
+  Serial.print("Sensor2: ");
+  Serial.print(floor(sensor1_value));
+  Serial.print("\t");
+  Serial.print("Glove Average: ");
+  Serial.print(glove_average);
+  Serial.print("\t");
+  Serial.print("Gripper degree: ");
+  Serial.print(gripper_value);
+  Serial.print("\t");
+  Serial.print("servo");
+  Serial.print(servo.read());
+  Serial.print("\t");
+  Serial.print("Roll:");
   Serial.print(roll_angle);
-  Serial.print(" ");
-  Serial.print("Pitch-Angle:");
-  Serial.print(pitch_angle);
-  Serial.print(" ");
-  Serial.print("Angle:");
-  Serial.print(sensor0_value);
-  Serial.print(" ");
-  Serial.print("Angle:");
-  Serial.print(sensor1_value);
-  Serial.print(" ");
-  //Serial.print(glove_average);
-  //Serial.print(" ");
-  Serial.print("Angle:");
-  Serial.print(gripper_degree);
-  Serial.println(); // Ensure to start a new line for the next set of data values
+  Serial.print("\n");
 
-  delay(200);
 }
