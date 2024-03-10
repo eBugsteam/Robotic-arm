edc8f225897499f91dc6bd1642b9e660655ea816
Taha Farshbaf <amirtahafarshbaf@gmail.com>
2024-03-10 14:05:35 +0330

    Update final.ino
    Add comments
    Add FSR 3 and 4
    Rewrite printing part

--- Codes/final.ino
+++ Codes/final.ino
@@ -1,37 +1,42 @@
+// Libraries
 #include <Wire.h>
 #include <Servo.h>
 #include <Adafruit_MPU6050.h>
 
+// Define pins
 #define sensor0 A0
 #define sensor1 A1
 #define sensor2 A2
 #define sensor3 A3
 
-double sensor0_value, sensor1_value, sensor2_value, sensor3_value, current_temp = 0;
 
-bool glove_wear = false;
-float glove_average = 0;
-float gripper_value = 0;
+// Global Variables
+double sensor0_value = 0, sensor1_value = 0, sensor2_value = 0, sensor3_value = 0, current_temp = 0;
+float glove_average = 0, gripper_value = 0, gripper_average = 0, glove_value = 0;
 int16_t roll_angle, pitch_angle;
-long unsigned int temp_count = 0;
+bool glove_wear = false;
 
+
+
+// Initializes Servo motors and Gyroscope
 Servo servo;
 Servo servo1;
 Servo servo2;
-
 Adafruit_MPU6050 mpu;
+
+
 void setup() {
-  // put your setup code here, to run once:
+
   Serial.begin(9600);
   Wire.begin();
   servo1.attach(3);
   servo2.attach(4);
   servo.attach(7);
   servo.write(170);
-  Serial.println("Initializing the mpu");
+  Serial.println("Initializing the MPU");
   Serial.println(mpu.begin() ? "Successfully Connected" : "Connection failed!");
   delay(1000);
-  Serial.println("Taking Values from the mpu");
+  Serial.println("Taking Values from the MPU");
   delay(1000);
   mpu.setAccelerometerRange(MPU6050_RANGE_16_G);
   mpu.setGyroRange(MPU6050_RANGE_250_DEG);
@@ -50,18 +55,22 @@ void loop() {
 
   servo1.write(roll_angle);
 
-  sensor0_value = floor(analogRead(sensor0) / 100);
-  sensor1_value = floor(analogRead(sensor1) / 100);
-  sensor2_value = analogRead(sensor2);
-  sensor3_value = analogRead(sensor3);
+  // Reading FSR values
+  sensor0_value = floor(analogRead(sensor0) / 100);  // Divide sensor value to 100 for getting stable data
+  sensor1_value = floor(analogRead(sensor1) / 100);  // Divide sensor value to 100 for getting stable data
+  sensor2_value = floor(analogRead(sensor2) / 100);  // Divide sensor value to 100 for getting stable data
+  sensor3_value = floor(analogRead(sensor3) / 100);  // Divide sensor value to 100 for getting stable data
 
-  glove_average = (sensor0_value + sensor1_value) / 2;
-  gripper_value = map(glove_average, 0, 4, 170, 10);
+  glove_average = (sensor0_value + sensor1_value) / 2;  // Calculate average value of two FSR
+  glove_value = map(glove_average, 0, 4, 170, 10);      // Map FSR value to servo motor rotation range(0-180)
 
-  servo.write(gripper_value);
+  gripper_average = (sensor2_value + sensor3_value) / 2;  // Calculate average value of two FSR
+  servo.write(glove_value);
 
+
+  // Receive data from blue module
   if (Serial.available() > 0) {
-   
+
     String str = Serial.readString();
     if (str.startsWith("S1")) {
       str = str.substring(3, str.length());
@@ -75,36 +84,36 @@ void loop() {
   }
 
 
-  if (temp.temperature - current_temp >= 0.09)
-    glove_wear = true;
+  // Glove_wearing detection
+  if (temp.temperature - current_temp >= 0.09) glove_wear = true;
   else glove_wear = false;
 
-  Serial.print("Sensor1: ");
-  Serial.print(floor(sensor0_value));
-  Serial.print("\t");
-  Serial.print("Sensor2: ");
-  Serial.print(floor(sensor1_value));
-  Serial.print("\t");
+
+  // Printting data
+  //Serial.print("Sensor1: ");
+  //Serial.print(floor(sensor0_value));
+  // Serial.print("\t");
+  //Serial.print("Sensor2: ");
+  // Serial.print(floor(sensor1_value));
+  // Serial.print("\t");
   Serial.print("Glove Average: ");
   Serial.print(glove_average);
   Serial.print("\t");
-  Serial.print("Gripper degree: ");
-  Serial.print(gripper_value);
+  Serial.print("Gripper Average: ");
+  Serial.print(gripper_average);
   Serial.print("\t");
   Serial.print("servo");
   Serial.print(servo.read());
   Serial.print("\t");
-  Serial.print("Roll:");
+  Serial.print("Twist degree:");
   Serial.print(roll_angle);
   Serial.print("\t");
   Serial.print("Temperature:");
   Serial.print(temp.temperature);
   Serial.print("\t");
-  Serial.print("Glove:");
+  Serial.print("Glove Status:");
   Serial.print(glove_wear);
   Serial.print("\n");
 
-  delay(30);
-
-  temp_count++;
+  delay(10);
 }
